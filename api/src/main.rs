bbd050c4088ce4b8ae372f6d3bc59a94c38f254b
M Gilang Januar <mgilangjanuar@gmail.com>
2025-03-13 22:36:01 +0700

    Add dotenv dependency and refactor prompt function to use environment variable for API key

--- api/src/main.rs
+++ api/src/main.rs
@@ -4,10 +4,12 @@ use axum::{
 	http::StatusCode,
 };
 use serde::{Deserialize, Serialize};
-use serde_json::{json, Value};
+use serde_json::{json, to_string};
+use dotenv::dotenv;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
   	let app = Router::new()
 		.route("/", get(
 			|| async { Json(json!({ "ping": 1 })) }
@@ -19,34 +21,36 @@ async fn main() {
 }
 
 async fn prompt(Json(payload): Json<PromptRequest>) -> (StatusCode, Json<PromptResponse>) {
-    let client = reqwest::Client::new();
-	let resp = client.post(format!("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent?key={}", "key"))
-        .body(json!({
-            "contents": [
-                {
-                    "role": "user",
-                    "parts": [
-                        {
-                            "text": payload.prompt
-                        }
-                    ]
-                },
-            ],
-            "generationConfig": {
-                "temperature": 0.7,
-                "maxOutputTokens": 8192,
-                "topP": 0.95,
-                "topK": 40,
-                "responseMimeType": "text/plain",
+    let api_key = std::env::var("GOOGLE_GENERATIVE_AI_API_KEY").expect("GOOGLE_GENERATIVE_AI_API_KEY must be set");
+    let body = AIRequest {
+        contents: vec![
+            AIContent {
+                role: "user".to_string(),
+                parts: vec![
+                    AIPart {
+                        text: payload.prompt,
+                    },
+                ],
             },
-        }).to_string())
+        ],
+        generationConfig: AIGenerationConfig {
+            temperature: 0.7,
+            maxOutputTokens: 8192,
+            topP: 0.95,
+            topK: 40,
+            responseMimeType: "text/plain".to_string(),
+        },
+    };
+
+    let client = reqwest::Client::new();
+	let resp = client.post(format!("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent?key={}", api_key))
+        .body(to_string(&body).unwrap())
         .header("Content-Type", "application/json")
         .send()
         .await;
-	let json = resp.unwrap().json::<Value>().await.unwrap();
-    println!("Response: {:?}", json);
-	let res = PromptResponse {
-		result: format!("Hello, {}!", payload.prompt),
+	let data = resp.unwrap().json::<AIResponse>().await.unwrap();
+    let res = PromptResponse {
+		result: data.candidates[0].content.parts[0].text.clone(),
 	};
 	(StatusCode::OK, Json(res))
 }
@@ -60,4 +64,50 @@ struct PromptRequest {
 #[derive(Serialize)]
 struct PromptResponse {
 	result: String,
+}
+
+#[derive(Serialize)]
+struct AIRequest {
+    contents: Vec<AIContent>,
+    generationConfig: AIGenerationConfig,
+}
+
+#[derive(Serialize)]
+struct AIContent {
+    role: String,
+    parts: Vec<AIPart>,
+}
+
+#[derive(Serialize)]
+struct AIPart {
+    text: String,
+}
+
+#[derive(Serialize)]
+struct AIGenerationConfig {
+    temperature: f32,
+    maxOutputTokens: i32,
+    topP: f32,
+    topK: i32,
+    responseMimeType: String,
+}
+
+#[derive(Deserialize)]
+struct AIResponse {
+    candidates: Vec<AICandidate>,
+}
+
+#[derive(Deserialize)]
+struct AICandidate {
+    content: AIContentCandidate,
+}
+
+#[derive(Deserialize)]
+struct AIContentCandidate {
+    parts: Vec<AIPartCandidate>,
+}
+
+#[derive(Deserialize)]
+struct AIPartCandidate {
+    text: String,
 }
