6867d70aa0846b9d272cf0e2eb114a96e37d0a38
M Gilang Januar <mgilangjanuar@gmail.com>
2025-03-13 22:05:46 +0700

    Add reqwest and serde dependencies; implement /prompt endpoint for content generation

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,17 +1,63 @@
 use axum::{
-  routing::get,
-  Router,
+  	routing::{get, post},
+  	Json, Router,
+	http::StatusCode,
 };
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
 
 #[tokio::main]
 async fn main() {
-  let app = Router::new()
-      .route("/", get(root));
+  	let app = Router::new()
+		.route("/", get(
+			|| async { Json(json!({ "ping": 1 })) }
+		))
+		.route("/prompt", post(prompt));
 
-  let listener = tokio::net::TcpListener::bind("0.0.0.0:4002").await.unwrap();
-  axum::serve(listener, app).await.unwrap();
+  	let listener = tokio::net::TcpListener::bind("0.0.0.0:4002").await.unwrap();
+  	axum::serve(listener, app).await.unwrap();
 }
 
-async fn root() -> &'static str {
-  "Hello, World!"
+async fn prompt(Json(payload): Json<PromptRequest>) -> (StatusCode, Json<PromptResponse>) {
+    let client = reqwest::Client::new();
+	let resp = client.post(format!("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent?key={}", "key"))
+        .body(json!({
+            "contents": [
+                {
+                    "role": "user",
+                    "parts": [
+                        {
+                            "text": payload.prompt
+                        }
+                    ]
+                },
+            ],
+            "generationConfig": {
+                "temperature": 0.7,
+                "maxOutputTokens": 8192,
+                "topP": 0.95,
+                "topK": 40,
+                "responseMimeType": "text/plain",
+            },
+        }).to_string())
+        .header("Content-Type", "application/json")
+        .send()
+        .await;
+	let json = resp.unwrap().json::<Value>().await.unwrap();
+    println!("Response: {:?}", json);
+	let res = PromptResponse {
+		result: format!("Hello, {}!", payload.prompt),
+	};
+	(StatusCode::OK, Json(res))
+}
+
+#[derive(Deserialize)]
+struct PromptRequest {
+	context: String,
+	prompt: String,
+}
+
+#[derive(Serialize)]
+struct PromptResponse {
+	result: String,
 }
